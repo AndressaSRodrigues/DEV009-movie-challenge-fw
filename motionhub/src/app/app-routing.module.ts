567a62c0b1a97e9d24a4a58d9f6585d2ba002c88
AndressaSRodrigues <srandressa2@gmail.com>
2023-10-02 14:50:48 -0300

    Created home, movies and tvshows components

--- motionhub/src/app/app-routing.module.ts
+++ motionhub/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tvshows', component: TvshowsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- motionhub/src/app/app.module.ts
+++ motionhub/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent,
+    TvshowsComponent
   ],
   imports: [
     BrowserModule,
