4ad62f3db28757e533c42431bbdfff9d0a6ac4b3
AndressaSRodrigues <srandressa2@gmail.com>
2023-10-13 12:29:28 -0300

    Reorganized directories

--- motionhub/src/app/app.module.ts
+++ motionhub/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { MoviesComponent } from './components/movies/movies.component';
-import { TvshowsComponent } from './components/tvshows/tvshows.component';
+import { HomeComponent } from './routes/home/home.component';
+import { MoviesComponent } from './routes/movies/movies.component';
+import { TvshowsComponent } from './routes/tvshows/tvshows.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchResultsComponent } from './routes/search-results/search-results.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
